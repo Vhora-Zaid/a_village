bfeeb196d153a6277ab4c61fb23695919afaf0a7
Zaid Vhora <vhoramohdzaid@gmail.com>
2025-03-10 14:51:16 +0530

    Questions Screen Contact Us Screen

--- lib/features/contact_us/contact_us_screen.dart
+++ lib/features/contact_us/contact_us_screen.dart
@@ -2,6 +2,7 @@ import 'package:a_village/common/widgets/app_appbar.dart';
 import 'package:a_village/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
+import '../../common/widgets/app_button.dart';
 import '../../utils/constants/app_fonts.dart';
 import '../../utils/constants/image_strings.dart';
 
@@ -13,60 +14,11 @@ class ContactUsScreen extends StatefulWidget {
 }
 
 class _ContactUsScreenState extends State<ContactUsScreen> {
+  final _formKey = GlobalKey<FormState>();
 
-  Widget _buildContactCard({
-    required String imagePath,
-    required String title,
-    required String subtitle,
-  }) {
-    return Card(
-      shadowColor: TColors.black,
-      color: TColors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      elevation: 1,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 15),
-        child: Row(
-          children: [
-            Image.asset(
-              imagePath,
-              height: 22,
-              width: 22,
-              color: TColors.blue,
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      color: TColors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: AppFonts.interregular,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    subtitle,
-                    style: TextStyle(
-                      color: TColors.grey,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: AppFonts.interregular,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -89,7 +41,7 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -102,13 +54,15 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
                   fontFamily: AppFonts.interbold,
                 ),
               ),
+              const SizedBox(height: 16),
               Center(
                 child: Image.asset(
                   ImageStrings.contactvector,
-                  height: 220,
-                  width: 220,
+                  height: 200,
+                  width: 200,
                 ),
               ),
+              const SizedBox(height: 12),
               Text(
                 'Please feel free to reach out if you have any questions, feedback, or need support.',
                 style: TextStyle(
@@ -120,27 +74,84 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 28),
-              _buildContactCard(
-                imagePath: ImageStrings.email,
-                title: 'Email',
-                subtitle: 'avillage@support.com',
-              ),
-              const SizedBox(height: 16),
-              _buildContactCard(
-                imagePath: ImageStrings.call,
-                title: 'Phone',
-                subtitle: '+1 (234) 567-8901',
-              ),
-              const SizedBox(height: 16),
-              _buildContactCard(
-                imagePath: ImageStrings.location,
-                title: 'Address',
-                subtitle: '123 Main Street,\nNew York, NY 10001',
-              ),
+
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    _buildInputField(
+                      controller: _emailController,
+                      label: 'Email Address',
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildInputField(
+                      controller: _subjectController,
+                      label: 'Subject',
+                      keyboardType: TextInputType.text,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildInputField(
+                      controller: _messageController,
+                      label: 'Message',
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 5,
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
-      )
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 30, top: 20, left: 16, right: 16),
+        child: AppButton(
+          title: 'Send Message',
+          onTap: () {
+            if (_formKey.currentState!.validate()) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Message sent!")),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInputField({
+    required TextEditingController controller,
+    required String label,
+    TextInputType keyboardType = TextInputType.text,
+    int maxLines = 1,
+  }) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      maxLines: maxLines,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter $label';
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          color: TColors.grey,
+          fontFamily: AppFonts.interregular,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: TColors.grey),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: TColors.blue),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      ),
     );
   }
 }
